2cbec5eff3e63ce8723f9fa6f6afd6a00b56e0f5
asportiello <132111455+asportiello@users.noreply.github.com>
2025-06-15 23:24:10 -0300

    Fix player sliding and adjust movement animations

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ const config: Phaser.Types.Core.GameConfig = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 600 },
+      gravity: { y: 0 },
       debug: false
     }
   },
